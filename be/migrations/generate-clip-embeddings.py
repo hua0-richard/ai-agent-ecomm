089260eae25c604ee5043268f94f6f80797a30af
Richard Hua <hua.richard0@gmail.com>
2026-04-03 15:00:30 -0700

    Better Copywriting

---
+++ be/migrations/generate-clip-embeddings.py
@@ -0,0 +1,6 @@
+from transformers import CLIPProcessor, CLIPModel
+from PIL import Image
+import torch
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
---
+++ be/migrations/get-hugging-face-data.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df = pd.read_parquet("hf://datasets/FronkonGames/steam-games-dataset/data/train-00000-of-00001.parquet")
